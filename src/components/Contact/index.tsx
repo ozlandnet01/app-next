c10ab4f2a40ea9e5f1241dd4a809388326a39f7f
agatha.settham <agatha.settham@gdn-commerce.com>
2023-05-07 21:26:22 +0700

    sosmed account

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -25,7 +25,7 @@ const Footer = () => {
             <iframe
                 width="100%"
                 height="100%"
-                src="https://www.youtube.com/embed/6OPwsqy06lM"
+                src="https://www.youtube.com/watch?v=yBT-fuF_mIY"
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
--- src/components/SocialMediaBar/index.tsx
+++ src/components/SocialMediaBar/index.tsx
@@ -7,11 +7,11 @@ interface Props {
 
 const SocialMediaBar = ({classNames} : Props)   => {
     return (
-    <div className={`${classNames} justify-between w-32 items-center`}> 
-        <div><FaInstagram /></div>
-        <div><FaTiktok /></div>
-        <div><FaTwitter /></div>
-        <div><FaYoutube /></div>
+    <div className={`${classNames} justify-between w-40 items-center`}> 
+        <div><FaInstagram onClick={() => {window.open('https://instagram.com/sodfestival?igshid=NTc4MTIwNjQ2YQ==')}} className="w-7 h-7 cursor-pointer"/></div>
+        <div><FaTiktok onClick={() => {window.open('https://www.tiktok.com/@sodfestival?_t=8c7B4CnVOle&_r=1')}} className="w-7 h-7 cursor-pointer"/></div>
+        <div><FaTwitter onClick={() => {window.open('https://twitter.com/sodfestival?s=21&t=KLf2POP_e5uImzVPbbePQQ')}} className="w-7 h-7 cursor-pointer"/></div>
+        <div><FaYoutube onClick={() => {window.open('https://www.tiktok.com/@sodfestival?_t=8c7B4CnVOle&_r=1')}} className="w-7 h-7 cursor-pointer"/></div>
       </div>
     )
   }
